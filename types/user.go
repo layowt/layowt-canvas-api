5f972513fe38c57c62e89cbb29ce663389689736
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-08-04 21:59:20 +0100

    separates types files

---
+++ types/user.go
@@ -0,0 +1,18 @@
+package types
+
+type User struct {
+	Uid                   int    `json:"uid"`
+	Email                 string `json:"email"`
+	HasAuthenticatedEmail bool   `json:"hasAuthenticatedEmail"`
+	FirstName             string `json:"firstName"`
+	LastName              string `json:"lastName"`
+	DisplayName           string `json:"displayName"`
+	CreatedAt             string `json:"createdAt"`
+	UpdatedAt             string `json:"updatedAt"`
+	LastLogin             string `json:"lastLogin"`
+	ExperienceLevel       string `json:"experienceLevel"`
+}
+
+type Admin interface {
+	DeleteUser() bool
+}
